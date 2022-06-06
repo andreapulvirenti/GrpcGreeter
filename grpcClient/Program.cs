c5077723e14c38f85979cd98b3df7c2e1ef0f5c6
Andrea Pulvirenti <an.pulvy@gmail.com>
2022-06-06 16:05:52 +0200

    Add project files.

---
+++ grpcClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcGreeterClient;
+
+// The port number must match the port of the gRPC server.
+using var channel = GrpcChannel.ForAddress("https://localhost:7195");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(
+                  new HelloRequest { Name = "Sono pulvi" });
+Console.WriteLine("Risposta: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
